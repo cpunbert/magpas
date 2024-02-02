dbb7694a7e886c600cdaa704d896104c7863dcc4
cpunbert <hubert.lisica@gmail.com>
2024-02-02 23:35:03 +0100

    input parse

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use std::env;
+
+
 fn main() {
-    println!("Hello, world!{:?}", "jdjdjdjdjdjdj");
+    println!("{:?}", "start");
+    parse_input();
+
 }
+
+
+fn parse_input(){
+    let args: Vec<String> = env::args().collect();
+
+    match args.len(){
+        1 => {
+            println!("jd1");
+        },
+        2 => {
+            let num = &args[1];
+            let number:i32 = match num.parse(){
+                Ok(n) =>{
+                    n
+                },
+                Err(_)=>{
+                    println!("jd2");
+                    return;
+                },
+
+            };
+            println!("{:?}", number)
+        },
+        _ => {
+            println!("chuj chuj");
+        }
+
+    }
+
+}
+
