35bff3d6ad0fb4f6cf737e3930537baed9f08050
cpunbert <hubert.lisica@gmail.com>
2024-02-05 00:02:00 +0100

    function generating random characters

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,33 @@
 use std::env;
-
+use rand::Rng;
 
 fn main() {
     println!("{:?}", "start");
     parse_input();
+    generate_password(4);
+
 
 }
+
+
+fn generate_password(pass_len: i32) {
+    let mut rng = rand::thread_rng();
+    let mut password: Vec<char> = Vec::new();
+    //make it the "rust-way"
+    for x in 0..pass_len{
+        match char::from_u32(rng.gen_range(65..90)){
+            Some(n) =>{
+                password.push(n);
+            },
+            None =>{
+                println!("jdjd");
+            }
+        }
+
+    }
+    println!("{:?}",password)
+}
+
 
 
 fn parse_input(){
